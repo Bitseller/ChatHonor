d777ad77aaaef761ed7a64147b8e21fd219005b0
Adrian Radice <radice.adrian.sjt@gmail.com>
2017-10-14 23:05:12 -0300

    CHAT

--- ChatServidor/src/Servidor/IMensajes.java
+++
@@ -1,8 +0,0 @@
-package Servidor;
-
-public interface IMensajes {
-	public final String FINALIZAR = ":q!";
-	public final char DESTINO_PUNTUAL = '@';
-	public final String FIN_CONEXION = ":q!->OK";
-	
-}
